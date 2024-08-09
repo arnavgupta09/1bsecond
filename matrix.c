5a33e7dd3904eb1bfce140cf49f9cb750cdac656
09-arnavgupta <09arnavgupta@gmail.com>
2024-08-10 00:38:07 +0530

    Added transpose function

--- matrix.c
+++ matrix.c
@@ -1,20 +1,26 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int main() {
     int n;
-    scanf("%d",&n);
+    printf("Enter the size of the matrix (n x n): ");
+    scanf("%d", &n);
+
     int arr[n][n];
 
     printf("Enter matrix elements: \n");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            scanf("%d",&arr[i][j]);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &arr[i][j]);
         }
     }
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            printf("%d",arr[i][j]);
+    printf("Transpose of the matrix: \n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", arr[j][i]);
         }
+        printf("\n");
     }
+
     return 0;
 }
